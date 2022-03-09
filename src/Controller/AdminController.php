3b5616254011580c5e56afbb9aa6c6dc27fe7ec5
montasser-7 <74327393+montasser-7@users.noreply.github.com>
2022-03-09 08:09:12 +0100

    integration user prod event commande + salle + cours + fullcalandar

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -42,11 +42,11 @@ class AdminController extends AbstractController
     /**
      * @Route("/admin/users", name="admin_users")
      */
-    public function searchUsers(UtilisateurRepository $rep ,Request $request, PaginatorInterface $paginator): Response
+    public function searchUsers(Request $request, PaginatorInterface $paginator): Response
     {
         $term = $request->get("term", false);
         if($term && $term !== ""){
-            $donnees = $rep->findEntitiesByString($term);
+            $donnees = $this->getDoctrine()->getRepository(Utilisateur::class)->findEntitiesByString($term);
         }else{
             $donnees = $this->getDoctrine()->getRepository(Utilisateur::class)->findBy([], ['nom' => 'desc']);
         }
